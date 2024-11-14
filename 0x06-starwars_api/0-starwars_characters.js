18db86f33ba1f97e37f99d5b9a048c68157bff09
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-11-14 12:11:19 -0800

    A script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,27 @@
 #!/usr/bin/node
 
-// Import the request module
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+    const characterURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+	  }
+          resolve(JSON.parse(charactersReqBody).name);
+	});
+      }));
 
-// Define the URL with the movie ID to fetch data from the Star Wars API
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// Make a request to the Star Wars API
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  
-  // Parse the JSON response body
-  const data = JSON.parse(body);
-  
-  // Extract the list of character URLs and fetch each character’s name
-  const characters = data.characters;
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-      
-      // Parse character data and log the name
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
+  Promise.all(charactersName)
+    .then(names => console.log(names.join('\n')))
+    .catch(allErr => console.log(allErr));
   });
-});
-
+}
